c220ba1f4ac2faf828f95a5c0b45d8dc642f5058
Maciej <cierpikowskimaciej@gmail.com>
2021-11-20 11:38:48 +0100

    configuration eslint, prettierrc, husky

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "require-jsdoc": 0,
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        line: {
+          markers: ["/"],
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
